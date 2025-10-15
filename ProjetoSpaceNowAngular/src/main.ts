35053fb2267007bf835e1c68c86488e50638648f
Debora Rosada <deborarosada12@gmail.com>
2025-10-15 17:29:27 -0300

    fix: main

--- ProjetoSpaceNowAngular/src/main.ts
+++ ProjetoSpaceNowAngular/src/main.ts
@@ -1,16 +1,16 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import { provideRouter } from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
-import { AppComponent } from "./app/app.component";
-import { HomeComponent } from "./app/components/home/home.component";
-import { LoginComponent } from "./app/components/login/login.component";
+// import { bootstrapApplication } from "@angular/platform-browser";
+// import { provideRouter } from "@angular/router";
+// import { provideHttpClient } from "@angular/common/http";
+// import { AppComponent } from "./app/app.component";
+// import { HomeComponent } from "./app/components/home/home.component";
+// import { LoginComponent } from "./app/components/login/login.component";
 
-const routes = [
-  { path: "", component: LoginComponent }, 
-  { path: "home", component: HomeComponent },
-  { path: "login", component: LoginComponent }, 
-];
+// const routes = [
+//   { path: "", component: LoginComponent }, 
+//   { path: "home", component: HomeComponent },
+//   { path: "login", component: LoginComponent }, 
+// ];
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient()],
-}).catch((err) => console.error(err));
+// bootstrapApplication(AppComponent, {
+//   providers: [provideRouter(routes), provideHttpClient()],
+// }).catch((err) => console.error(err));
